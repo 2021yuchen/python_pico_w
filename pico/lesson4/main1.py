3ea6ce808a218ac121601f56df221418bc216642
2021yuchen <a0981010258@gmail.com>
2024-09-21 11:49:37 +0800

    modify

--- pico/lesson4/main1.py
+++ pico/lesson4/main1.py
@@ -1,1 +1,24 @@
-print("Hello! Python from micropython")
+#print("Hello! Python from micropython")
+
+#from machine import Pin
+#led = Pin("LED", Pin.OUT)
+#led.value(0)
+
+#from machine import Pin
+#led = Pin("LED", mode=Pin.OUT)
+#led.off()
+
+
+from machine import Pin
+import time
+
+led = Pin("LED", mode=Pin.OUT)
+status = False
+while True:
+    if status == False:
+        led.on()
+        status = True
+    else:
+        led.off()
+        status = False
+    time.sleep(1)
